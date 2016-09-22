3b3a6e39c9ddaef523ee0a4ea7f82c09a2741ae7
rg <rahul.gaur@innovaccer.com>
2016-09-23 01:48:06 +0530

    fixes

--- rc.lua
+++ rc.lua
@@ -845,7 +845,7 @@ client.connect_signal("manage", function (c, startup)
          end
      end)
 
-    local titlebars_enabled = false
+    local titlebars_enabled = true
     if titlebars_enabled and (c.type == "normal" or c.type == "dialog") then
         -- buttons for the titlebar
         local buttons = awful.util.table.join(
